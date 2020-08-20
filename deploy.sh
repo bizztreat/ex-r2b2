401500a5d7a0a7d72eb66baf5049f7de10e34694
Tomas Votava <tomas.votava@bizztreat.com>
2020-08-20 14:55:05 +0200

    travis from gitlab

--- deploy.sh
+++ deploy.sh
@@ -15,9 +15,9 @@ eval $(docker run --rm \
     ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP})
 
 # Push to the repository
-docker tag ${APP_IMAGE}:latest ${REPOSITORY}:${CI_COMMIT_TAG}
+docker tag ${APP_IMAGE}:latest ${REPOSITORY}:${TRAVIS_TAG}
 docker tag ${APP_IMAGE}:latest ${REPOSITORY}:latest
-docker push ${REPOSITORY}:${CI_COMMIT_TAG}
+docker push ${REPOSITORY}:${TRAVIS_TAG}
 docker push ${REPOSITORY}:latest
 
 # Update the tag in Keboola Developer Portal -> Deploy to KBC
@@ -27,7 +27,7 @@ then
         -e KBC_DEVELOPERPORTAL_USERNAME \
         -e KBC_DEVELOPERPORTAL_PASSWORD \
         quay.io/keboola/developer-portal-cli-v2:latest \
-        update-app-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} ${CI_COMMIT_TAG} ecr ${REPOSITORY}
+        update-app-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP} ${TRAVIS_TAG} ecr ${REPOSITORY}
 else
-    echo "Skipping deployment to KBC, tag ${CI_COMMIT_TAG} is not allowed."
+    echo "Skipping deployment to KBC, tag ${TRAVIS_TAG} is not allowed."
 fi
